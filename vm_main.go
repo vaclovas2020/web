7050f09775b6ca512d01d9388a46697282d85940
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-01-29 19:06:02 +0200

    added done chan

--- vm_main.go
+++ vm_main.go
@@ -7,7 +7,7 @@ import (
 )
 
 /* Initialize VM with given context and arguments. Please provide correct sourceDir - directory of Web language source files */
-func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string, output chan<- string, errCh chan<- string) {
+func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string, output chan<- string, errCh chan<- string, done chan<- bool) {
 	wg := &sync.WaitGroup{}
 	files, err := ioutil.ReadDir(sourceDir)
 	if err != nil {
@@ -19,13 +19,14 @@ func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string, outpu
 			go vm.loadFile(wg, file.Name(), ctx, output, errCh)
 		}
 	}
-	go wait(wg, output, errCh)
+	go wait(wg, output, errCh, done)
 }
 
-func wait(wg *sync.WaitGroup, output chan<- string, err chan<- string) {
+func wait(wg *sync.WaitGroup, output chan<- string, err chan<- string, done chan<- bool) {
 	wg.Wait()
 	close(err)
 	close(output)
+	done <- true
 }
 
 /* load source file from disk. Still not yet fully implemented */
