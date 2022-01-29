42672c707d13efa469fa68334c52c8d4edf567df
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-01-29 20:43:46 +0200

    added parseDone channel

--- vm_main.go
+++ vm_main.go
@@ -13,6 +13,7 @@ func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string) {
 	output := make(chan string)
 	errCh := make(chan string)
 	files, err := ioutil.ReadDir(sourceDir)
+	parseDone := make(chan bool, 1)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -22,31 +23,32 @@ func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string) {
 			go vm.loadFileWorker(wg, file.Name(), ctx, output, errCh)
 		}
 	}
-	go vm.monitorWorker(wg, output, errCh)
+	go vm.monitorWorker(wg, output, errCh, parseDone)
 	done := make(chan bool, 1)
-	go vm.printWorker(output, errCh, done)
+	go vm.printWorker(output, errCh, done, parseDone)
 	<-done
 }
 
 /* print output and/or error to screen */
-func (vm *VM) printWorker(output <-chan string, errCh <-chan string, done chan<- bool) {
+func (vm *VM) printWorker(output <-chan string, errCh <-chan string, done chan<- bool, parseDone <-chan bool) {
 	for {
 		select {
 		case <-output:
 			fmt.Println(<-output)
 		case <-errCh:
 			fmt.Println("Fatal error: " + <-errCh)
-		default:
-			done <- true
+		case <-parseDone:
+			done <- <-parseDone
 		}
 	}
 }
 
 /* wait until all workers finish and close channels */
-func (vm *VM) monitorWorker(wg *sync.WaitGroup, output chan<- string, errCh chan<- string) {
+func (vm *VM) monitorWorker(wg *sync.WaitGroup, output chan<- string, errCh chan<- string, done chan<- bool) {
 	wg.Wait()
 	close(errCh)
 	close(output)
+	done <- true
 }
 
 /* load source file from disk. Still not yet fully implemented */
