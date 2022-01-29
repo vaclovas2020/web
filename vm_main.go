8b9488eba606541c8165e8d4817a87f618025f50
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-01-29 18:42:27 +0200

    bug fixes: goroutine deadlock

--- vm_main.go
+++ vm_main.go
@@ -2,34 +2,34 @@ package web
 
 import (
 	"context"
-	"errors"
 	"io/ioutil"
-	"log"
+	"sync"
 )
 
 /* Initialize VM with given context and arguments. Please provide correct sourceDir - directory of Web language source files */
-func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string, output chan<- string) error {
-	errCh := make(chan string)
+func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string, output chan<- string, errCh chan<- string) {
+	wg := &sync.WaitGroup{}
 	files, err := ioutil.ReadDir(sourceDir)
 	if err != nil {
-		log.Fatal(err)
+		errCh <- err.Error()
 	}
 	for _, file := range files {
 		if !file.IsDir() {
-			go vm.loadFile(file.Name(), ctx, output, errCh)
-		}
-		select {
-		case <-errCh:
-			return errors.New("Fatal error: " + <-errCh)
-		case <-ctx.Done():
-			return ctx.Err()
+			wg.Add(1)
+			go vm.loadFile(wg, file.Name(), ctx, output, errCh)
 		}
 	}
-	return nil
+	go wait(wg, output, errCh)
+}
+
+func wait(wg *sync.WaitGroup, output chan<- string, err chan<- string) {
+	wg.Wait()
+	close(err)
+	close(output)
 }
 
 /* load source file from disk. Still not yet fully implemented */
-func (vm *VM) loadFile(fileName string, ctx context.Context, output chan<- string, err chan<- string) {
+func (vm *VM) loadFile(wg *sync.WaitGroup, fileName string, ctx context.Context, output chan<- string, err chan<- string) {
 	data, error := ioutil.ReadFile(fileName)
 	if error != nil {
 		err <- error.Error()
