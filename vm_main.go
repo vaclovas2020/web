7e136138678256b20f4299e12b0491517da6aa00
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-01-29 19:58:17 +0200

    test case

--- vm_main.go
+++ vm_main.go
@@ -43,7 +43,7 @@ func (vm *VM) printWorker(output <-chan string, err <-chan string, done chan<- b
 }
 
 /* wait until all workers finish and close channels */
-func (vm *VM) monitorWorker(wg *sync.WaitGroup, output chan string, errCh chan string) {
+func (vm *VM) monitorWorker(wg *sync.WaitGroup, output chan<- string, errCh chan<- string) {
 	wg.Wait()
 	close(errCh)
 	close(output)
@@ -52,11 +52,7 @@ func (vm *VM) monitorWorker(wg *sync.WaitGroup, output chan string, errCh chan s
 /* load source file from disk. Still not yet fully implemented */
 func (vm *VM) loadFileWorker(wg *sync.WaitGroup, fileName string, ctx context.Context, output chan<- string, errCh chan<- string) {
 	defer wg.Done()
-	data, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		errCh <- err.Error()
-	}
-	output <- string(data)
 	vm.classes = make(map[string]Class)
+	output <- fileName
 	// TODO: parse source and create Class struct array
 }
