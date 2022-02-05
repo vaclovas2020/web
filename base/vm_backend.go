63ca3cbce095d1b1c4176a9c8ba685e6aeb9a544
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-05 22:09:58 +0200

    added FunctionHandler

--- base/vm_backend.go
+++ base/vm_backend.go
@@ -2,11 +2,15 @@ package base
 
 /* Web function representation in VM */
 type Function struct {
-	Args map[string]interface{}
+	Args    map[string]*interface{}
+	Handler *FunctionHandler
 }
 
 /* Web class represantation in VM */
 type Class struct {
-	Attributes map[string]interface{}
+	Attributes map[string]*interface{}
 	Methods    map[string]Function
 }
+
+/* User defined function handler */
+type FunctionHandler func(args map[string]*interface{}) (*interface{}, error)
