1160107da3005dd0091ac60c4d13716fb303fc67
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-05 23:01:03 +0200

    load source files recursive

--- vm_main.go
+++ vm_main.go
@@ -15,30 +15,38 @@ import (
 type VM struct {
 	classes map[string]base.Class
 	parser  *parser.Parser
+	wg      *sync.WaitGroup
 }
 
 /* Initialize VM with given context and arguments. Please provide correct sourceDir - directory of Web language source files */
 func (vm *VM) InitVM(ctx context.Context, args []string, sourceDir string) {
 	vm.classes = make(map[string]base.Class)
 	vm.parser = &parser.Parser{Classes: &vm.classes}
-	wg := &sync.WaitGroup{}
+	vm.wg = &sync.WaitGroup{}
 	count := 0
+	output := make(chan string)
+	vm.loadSourceDir(&count, ctx, sourceDir, output)
+	go vm.monitorWorker(vm.wg, output)
+	done := make(chan bool, 1)
+	go vm.printWorker(count, output, done)
+	<-done
+}
+
+/* parse source file */
+func (vm *VM) loadSourceDir(count *int, ctx context.Context, sourceDir string, output chan<- string) {
 	files, err := ioutil.ReadDir(sourceDir)
 	if err != nil {
 		panic(err.Error())
 	}
-	output := make(chan string)
 	for _, file := range files {
 		if !file.IsDir() {
-			wg.Add(1)
-			go vm.parseFileWorker(wg, fmt.Sprintf("%v/%v", sourceDir, file.Name()), ctx, output)
-			count++
+			vm.wg.Add(1)
+			go vm.parseFileWorker(vm.wg, fmt.Sprintf("%v/%v", sourceDir, file.Name()), ctx, output)
+			*count++
+		} else {
+			vm.loadSourceDir(count, ctx, fmt.Sprintf("%v/%v", sourceDir, file.Name()), output)
 		}
 	}
-	go vm.monitorWorker(wg, output)
-	done := make(chan bool, 1)
-	go vm.printWorker(count, output, done)
-	<-done
 }
 
 /* print output and/or error to screen */
