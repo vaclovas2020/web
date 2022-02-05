f18a0de89724e1ea99a293d4f4dce56e3b2d10dd
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-06 01:51:30 +0200

    implemented controller

--- controller/controller_handler.go
+++ controller/controller_handler.go
@@ -1,12 +1,40 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+
+	app "webimizer.dev/webimizer"
 )
 
 type Handler func(rw http.ResponseWriter, r *http.Request)
 
 func (handler Handler) Invoke(args map[string]interface{}) error {
-	// TODO: Invoke handler
+	if v, found := args["route"]; found {
+		if method, found := args["method"]; found {
+			methodStr := fmt.Sprintf("%v", method)
+			http.Handle(fmt.Sprintf("%v", v), app.HttpHandler(func(rw http.ResponseWriter, r *http.Request) {
+				switch methodStr {
+				case http.MethodHead:
+					app.Head(rw, r, app.IfHttpMethodHandler(handler))
+				case http.MethodGet:
+					app.Get(rw, r, app.IfHttpMethodHandler(handler))
+				case http.MethodPost:
+					app.Post(rw, r, app.IfHttpMethodHandler(handler))
+				case http.MethodPut:
+					app.Put(rw, r, app.IfHttpMethodHandler(handler))
+				case http.MethodDelete:
+					app.Delete(rw, r, app.IfHttpMethodHandler(handler))
+				case http.MethodOptions:
+					app.Options(rw, r, app.IfHttpMethodHandler(handler))
+				}
+			}))
+		} else {
+			http.Handle(fmt.Sprintf("%v", v), app.HttpHandler(handler))
+		}
+	} else {
+		return errors.New("controller has no route defined")
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module webimizer.dev/web
 
 go 1.17
+
+require webimizer.dev/webimizer v1.2.0 // indirect
