0169c067e6e126f10adeb97f120ed2ac82424f0a
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-06 05:14:07 +0200

    added parser

--- base/vm_backend.go
+++ base/vm_backend.go
@@ -2,14 +2,15 @@ package base
 
 /* Web function representation in VM */
 type Function struct {
-	Args    map[string]*interface{}
+	Args    map[string]interface{}
 	Handler FunctionHandler
 }
 
 /* Web class represantation in VM */
 type Class struct {
-	Attributes map[string]*interface{}
+	Attributes map[string]interface{}
 	Methods    map[string]Function
+	Type       string
 }
 
 /* User defined function handler */
--- parser/weblang_parser.go
+++ parser/weblang_parser.go
@@ -1,12 +1,82 @@
 package parser
 
-import "webimizer.dev/web/base"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
 
+	"webimizer.dev/web/base"
+)
+
+/* Weblang language syntax parser */
 type Parser struct {
 	Classes *map[string]base.Class
 }
 
+const regExpClassName string = "[[:alpha:]]\\w*"
+const serverRegExpStart string = "^(server)\\s+" + regExpClassName + "\\s*[{]\\s*"
+const serverRegExpParamName = "(router|port|host)"
+const serverRegExpOneParam string = "([@]" + serverRegExpParamName + "[(]\\s+[\"]*(\\w|[.])+[\"]*\\s+[)]\\s+)"
+const serverRegExpParams string = serverRegExpOneParam + "{3}"
+const serverRegExpEnd string = "[}]\\s*$"
+const serverRegExpFull string = serverRegExpStart + serverRegExpParams + serverRegExpEnd
+
+/* Parse source code and append result to class map */
 func (parser *Parser) Parse(sourceCode string) error {
-	// TODO: implement parser
+	if parser.isServerClass(sourceCode) {
+		err := parser.parseServer(sourceCode)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func (parser *Parser) pushToMap(className string, class *base.Class) error {
+	if _, found := (*parser.Classes)[className]; !found {
+		(*parser.Classes)[className] = *class
+		return nil
+	}
+	return fmt.Errorf("class with name '%v' already exists", className)
+}
+
+func (parser *Parser) parseServerParams(class *base.Class, sourceCode string) {
+	serverExpOneParam := parser.compileRegExp(serverRegExpOneParam)
+	if serverExpOneParam.MatchString(sourceCode) {
+		oneParam := serverExpOneParam.FindString(sourceCode)
+		newSourceCode := strings.Replace(sourceCode, oneParam, "", 1)
+		// TODO: parse param name and value
+		if newSourceCode != "" {
+			parser.parseServerParams(class, newSourceCode)
+		}
+	}
+}
+
+func (parser *Parser) parseServer(sourceCode string) error {
+	var className string
+	var class base.Class
+	serverExpFull := parser.compileRegExp(serverRegExpFull)
+	if serverExpFull.MatchString(sourceCode) {
+		serverExpStart := parser.compileRegExp(serverRegExpStart)
+		classNameExp := parser.compileRegExp(regExpClassName)
+		className = classNameExp.FindString(strings.Replace(serverExpStart.FindString(sourceCode), "server", "", 1))
+		parser.parseServerParams(&class, sourceCode)
+	} else {
+		return errors.New("incorrect class definition syntax")
+	}
+	return parser.pushToMap(className, &class)
+}
+
+func (parser *Parser) compileRegExp(regExp string) *regexp.Regexp {
+	serverExpStart, err := regexp.Compile(regExp)
+	if err != nil {
+		panic(err)
+	}
+	return serverExpStart
+}
+
+func (parser *Parser) isServerClass(sourceCode string) bool {
+	serverExpStart := parser.compileRegExp(serverRegExpStart)
+	return serverExpStart.MatchString(sourceCode)
 }
