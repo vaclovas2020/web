3cca761b870752845ae7d6671ff772043cda4234
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-06 02:08:26 +0200

    added server

---
+++ server/server.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"webimizer.dev/web/base"
+)
+
+type Server struct {
+	Host        string
+	Port        int
+	ServerClass *base.Class
+	RouterClass *base.Class
+}
+
+func (sr Server) Start() {
+	log.Printf("\033[32m[weblang]\033[0m Server starting listen on %v:%v...", sr.Host, sr.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", sr.Host, sr.Port), nil))
+}
