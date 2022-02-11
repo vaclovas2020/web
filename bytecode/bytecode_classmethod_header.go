03f0a2c858d1362576d074a9f1d9285882ef033f
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-11 19:10:43 +0200

    added ClassName and ClassType

--- bytecode/bytecode_classmethod_header.go
+++ bytecode/bytecode_classmethod_header.go
@@ -2,7 +2,7 @@ package bytecode
 
 /* Header struct for classMethod declaration in Weblang bytecode file */
 type ClassMethodHeader struct {
-	ClassName                [80]byte // Class name
+	MethodName               [80]byte // Class method name (max 80 symbols)
 	InstructionSetStartIndex uint64   // Index of first InstructionSet block
 	InstructionSetEndIndex   uint64   // Index of last InstructionSet block
 }
--- bytecode/bytecode_header.go
+++ bytecode/bytecode_header.go
@@ -10,10 +10,12 @@ const ByteCodeVersion uint16 = 0x0001
 
 /* Header struct for Weblang bytecode class file */
 type ClassHeader struct {
-	FileFormatText      [8]byte // First 8 bytes of file. It's contains "WEBLANG\x0f"
-	ByteCodeVersion     uint16  // Version of bytecode file
-	InstructionSetCount uint64  // Count of InstructionSet struct
-	ClassMethodsCount   uint64  // Count of ClassMethodHeader struct
+	FileFormatText      [8]byte  // First 8 bytes of file. It's contains "WEBLANG\x0f"
+	ClassName           [80]byte // Class name (max 80 symbols)
+	ClassType           uint8    // Class type: server, controller, router, model, view
+	ByteCodeVersion     uint16   // Version of bytecode file
+	InstructionSetCount uint64   // Count of InstructionSet struct
+	ClassMethodsCount   uint64   // Count of ClassMethodHeader struct
 }
 
 /* Write header data to struct. Use to prepare for write header struct to file */
