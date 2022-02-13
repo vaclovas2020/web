981435f25c37540760df3a21b27c6170ba356236
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-13 14:25:51 +0200

    update method types

--- bytecode/class/method/method_types.go
+++ bytecode/class/method/method_types.go
@@ -1,10 +1,19 @@
 package method
 
 /* Method type public: publically accessible class method */
-
 const MethodType_Public uint8 = 0x01
+
+/* Method type private: private accessible class method */
 const MethodType_Private uint8 = 0x02
+
+/* Method type protected: protected accessible class method */
 const MethodType_Protected uint8 = 0x03
+
+/* Method type external public: external (use with vm.DefineFunc() ) and public accessible class method */
 const MethodType_ExternalPublic uint8 = 0x04
+
+/* Method type external public: external (use with vm.DefineFunc() ) and private accessible class method */
 const MethodType_ExternalPrivate uint8 = 0x05
+
+/* Method type external public: external (use with vm.DefineFunc() ) and protected accessible class method */
 const MethodType_ExternalProtected uint8 = 0x06
