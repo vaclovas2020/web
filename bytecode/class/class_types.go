fe14f27a00ffe90bb6db259f086d7010c37d64ed
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-16 21:54:22 +0200

    added new class type firewall

--- bytecode/class/class_types.go
+++ bytecode/class/class_types.go
@@ -5,26 +5,29 @@ package class
 /* Weblang build-in class type object */
 const ClassType_Object uint8 = 0x00
 
-/* Weblang buil-in class type server */
+/* Weblang built-in class type server */
 const ClassType_Server uint8 = 0x01
 
-/* Weblang buil-in class type controller */
+/* Weblang built-in class type controller */
 const ClassType_Controller uint8 = 0x02
 
-/* Weblang buil-in class type router */
+/* Weblang built-in class type router */
 const ClassType_Router uint8 = 0x03
 
-/* Weblang buil-in class type model */
+/* Weblang built-in class type model */
 const ClassType_Model uint8 = 0x04
 
-/* Weblang buil-in class type repository */
+/* Weblang built-in class type repository */
 const ClassType_Repository uint8 = 0x05
 
-/* Weblang buil-in class type service */
+/* Weblang built-in class type service */
 const ClassType_Service uint8 = 0x06
 
-/* Weblang buil-in class type view */
+/* Weblang built-in class type view */
 const ClassType_View uint8 = 0x07
 
-/* Weblang buil-in class type cms */
+/* Weblang built-in class type cms */
 const ClassType_Cms uint8 = 0x08
+
+/* Weblang built-in class type firewall */
+const ClassType_Firewall uint8 = 0x09
