382d7b55bcfa09599dd4e7ba85ad70bbe666bfab
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-02-20 17:59:20 +0200

    cms error fix

--- core/cms/cms.go
+++ core/cms/cms.go
@@ -16,13 +16,14 @@ type CMS struct {
 	AdminUrl string
 }
 
+// content is our static web server content.
+//go:embed static/*
+var content embed.FS
+
 func (cms *CMS) ServeStaticFiles() error {
 	if cms.AdminUrl == "" {
 		return errors.New("admin url cannot be empty string")
 	}
-	// content is our static web server content.
-	//go:embed static/*
-	var content embed.FS
 	http.Handle(cms.AdminUrl,
 		http.StripPrefix(cms.AdminUrl,
 			webimizer.HttpHandler(func(rw http.ResponseWriter, r *http.Request) { http.FileServer(http.FS(content)).ServeHTTP(rw, r) })))
--- vm_main.go
+++ vm_main.go
@@ -17,7 +17,7 @@ import (
 )
 
 /* Weblang version string */
-const Version string = "v0.4.12"
+const Version string = "v0.4.13"
 
 /* Main VM struct */
 type VM struct {
