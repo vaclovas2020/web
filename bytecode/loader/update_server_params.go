c4ea555e7250063651c28d1775e5d2bd9bf48804
vaclovas2020 <vaclovas.lapinskis@gmail.com>
2022-03-12 10:49:41 +0200

    detect wehen server is nil

--- bytecode/loader/update_server_params.go
+++ bytecode/loader/update_server_params.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (loader *Loader) updateServerParams(obj *base.Object) {
-	if obj != nil && obj.Class != nil && obj.Class.Type == "server" {
+	if loader.Server != nil && obj != nil && obj.Class != nil && obj.Class.Type == "server" {
 		(*loader.Server).ServerObject = obj
 		(*loader.Server).Host = fmt.Sprintf("%v", obj.Attributes["host"])
 		(*loader.Server).Port = int(obj.Attributes["port"].(int64))
